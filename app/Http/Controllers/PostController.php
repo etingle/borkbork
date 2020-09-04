32664fba32ad60ebb04dde3fe30afc53ffab1f5e
Evan Tingle <evan.tingle@gmail.com>
2020-09-04 14:38:32 -0400

    save iamges

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -125,7 +125,7 @@ curl_close($curl);
 
 $contents=file_get_contents($url);
 
-Storage::disk('public')->put($post->created_at->format('m-d-Y_H_i_s).'_'.$i.'.jpg',$contents);
+Storage::disk('public')->put($post->created_at->format('m-d-Y_H_i_s').'_'.$i.'.jpg',$contents);
 
 //Storage::disk('public')->put('test2.jpg',$contents);
 
