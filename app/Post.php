97f06c27ddaf3d5ba160e6f6c73688f1bd8024fc
Evan Tingle <evan.tingle@gmail.com>
2020-09-19 12:03:30 -0400

    date selection

--- app/Post.php
+++ app/Post.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Post extends Model
 {
 
@@ -17,6 +17,26 @@ class Post extends Model
 	public function images(){
 		return $this->hasMany('App\Image');
 	} 
+
+       public static function dates(){
+		$dates=DB::select("select distinct YEAR(created_at) as year, MONTHNAME(created_at) as monthname, MONTH(created_at) as month from posts order by year DESC, month DESC");
+		$dates_array=[];
+
+$i=0;
+foreach ($dates as $date){
+        if(($i>0) and ($date->year==$dates_array[$i-1][0])){
+                array_push($dates_array[$i-1],array($date->monthname,$date->month));
+
+        } else {
+                array_push($dates_array,array($date->year));
+		array_push($dates_array[$i],array($date->monthname,$date->month));
+                $i=$i+1;
+        }
+}
+
+                return $dates_array;
+        }
+
 	
 
 }
